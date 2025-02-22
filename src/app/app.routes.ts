9af7d92c181996362f7711b1b7db49eedf6f2013
thamiresimpalea <thamicoiado.26@gmail.com>
2025-02-22 19:27:19 -0300

    create routes for home, recipe, sweet and savory pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'salgados',
+    loadComponent: () => import('./savory/savory.component').then(m => m.SavoryComponent)
+  },
+  {
+    path: 'doces',
+    loadComponent: () => import('./sweet/sweet.component').then(m => m.SweetComponent)
+  },
+  {
+    path: 'receita/:id',
+    loadComponent: () => import('./recipe/recipe.component').then(m => m.RecipeComponent)
+  }
+];
