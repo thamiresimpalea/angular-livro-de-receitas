05545fcae4649cc12d602c6c0f1dfc805f74bdbc
thamiresimpalea <thamicoiado.26@gmail.com>
2025-02-22 19:31:38 -0300

    create recipes service to call apis

---
+++ src/app/services/recipes.service.ts
@@ -0,0 +1,18 @@
+import { inject, Injectable } from '@angular/core';
+import { RecipeItem } from '../model/recipe-item';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+  http = inject(HttpClient);
+  getRecipes() {
+    const url = 'https://api-receitas-pi.vercel.app/receitas/todas';
+    return this.http.get<Array<RecipeItem>>(url);
+  }
+  getRecipeById(id: string) {
+    const url = 'https://api-receitas-pi.vercel.app/receitas/'+id;
+    return this.http.get<RecipeItem>(url);
+  }
+}
