ceca4d9178774f1bfcead5376405e8c93de5b7b8
thamiresimpalea <thamicoiado.26@gmail.com>
2025-03-30 17:50:55 -0300

    adding savory recipe list

--- src/app/savory/savory.component.ts
+++ src/app/savory/savory.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { RecipesService } from '../services/recipes.service';
+import { RecipeItem } from '../model/recipe-item';
+import { catchError } from 'rxjs';
+import { RecipeItemComponent } from '../components/recipe-item/recipe-item.component';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-savory',
-  imports: [],
+  imports: [RecipeItemComponent],
   templateUrl: './savory.component.html',
   styleUrl: './savory.component.scss'
 })
 export class SavoryComponent {
+  recipeService = inject(RecipesService);
+  allRecipeItems = signal<Array<RecipeItem>>([]);
+  error = signal<string>('');
 
+  ngOnInit(): void {
+    this.recipeService
+      .getRecipesByType('salgado')
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          console.error('Error loading recipes', error);
+          this.error.set(error.message)
+          throw error;
+        })
+      )
+      .subscribe((recipes) => {
+        this.allRecipeItems.set(recipes);
+      });
+  }
 }
