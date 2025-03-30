325bde01f930d35609b7292b6386945a13deae67
thamiresimpalea <thamicoiado.26@gmail.com>
2025-03-30 13:40:12 -0300

    creating functionality for sweet component

--- src/app/sweet/sweet.component.ts
+++ src/app/sweet/sweet.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { RecipesService } from '../services/recipes.service';
+import { RecipeItem } from '../model/recipe-item';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-sweet',
@@ -7,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrl: './sweet.component.scss'
 })
 export class SweetComponent {
+  recipeService = inject(RecipesService);
+  allRecipeItems = signal<Array<RecipeItem>>([]);
+  error = signal<string>('');
 
+  ngOnInit(): void {
+    this.recipeService
+      .getRecipesByType('doce')
+      .pipe(
+        catchError((error) => {
+          console.error('Error loading recipes', error);
+          throw error;
+        })
+      )
+      .subscribe((recipes) => {
+        this.allRecipeItems.set(recipes);
+      });
+  }
 }
