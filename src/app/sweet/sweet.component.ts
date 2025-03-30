22ba6e0863f47037361f316468727022b0d1b3d2
thamiresimpalea <thamicoiado.26@gmail.com>
2025-03-30 17:50:38 -0300

    adding sweet recipe list

--- src/app/sweet/sweet.component.ts
+++ src/app/sweet/sweet.component.ts
@@ -2,10 +2,12 @@ import { Component, inject, signal } from '@angular/core';
 import { RecipesService } from '../services/recipes.service';
 import { RecipeItem } from '../model/recipe-item';
 import { catchError } from 'rxjs';
+import { RecipeItemComponent } from '../components/recipe-item/recipe-item.component';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-sweet',
-  imports: [],
+  imports: [RecipeItemComponent],
   templateUrl: './sweet.component.html',
   styleUrl: './sweet.component.scss'
 })
@@ -18,8 +20,9 @@ export class SweetComponent {
     this.recipeService
       .getRecipesByType('doce')
       .pipe(
-        catchError((error) => {
+        catchError((error: HttpErrorResponse) => {
           console.error('Error loading recipes', error);
+          this.error.set(error.message)
           throw error;
         })
       )
